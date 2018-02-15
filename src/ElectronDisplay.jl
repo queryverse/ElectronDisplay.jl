954b85ad0b50984a59ea07f3b4fb16b8a5f4c10f
David Anthoff <anthoff@berkeley.edu>
2018-02-14 17:39:59 -0800

    Enable more MIME types

--- src/ElectronDisplay.jl
+++ src/ElectronDisplay.jl
@@ -6,46 +6,84 @@ using Electron
 struct ElectronDisplayType <: Base.Display end
 
 
-# function Base.display(d::ElectronDisplayType, ::MIME{Symbol("image/png")}, x)
-#     payload = stringmime(MIME("image/png"), x)
-#     print(conn, "image/png", ":", endof(payload), ";")
-#     print(conn, payload)
-# end
+function Base.display(d::ElectronDisplayType, ::MIME{Symbol("image/png")}, x)
+    img = stringmime(MIME("image/png"), x)
 
-# Base.displayable(d::ElectronDisplayType, ::MIME{Symbol("image/png")}) = true
+    payload = string("<img src=\"data:image/png;base64,", img, "\"/>")
+
+    w = Electron.Window(payload)
+end
+
+Base.displayable(d::ElectronDisplayType, ::MIME{Symbol("image/png")}) = true
 
 function Base.display(d::ElectronDisplayType, ::MIME{Symbol("image/svg+xml")}, x)
-    w = Electron.Window()
-
     payload = stringmime(MIME("image/svg+xml"), x)
 
-    body!(w, payload)
+    w = Electron.Window(payload)
 end
 
 Base.displayable(d::ElectronDisplayType, ::MIME{Symbol("image/svg+xml")}) = true
 
-# function Base.display(d::ElectronDisplayType, ::MIME{Symbol("text/html")}, x)
-#     payload = stringmime(MIME("text/html"), x)
-#     print(conn, "text/html", ":", endof(payload), ";")
-#     print(conn, payload)
-# end
+asset(url...) = replace(normpath(joinpath(@__DIR__, "..", "assets", "vega", url...)), "\\", "/")
 
-# Base.displayable(d::ElectronDisplayType, ::MIME{Symbol("text/html")}) = true
+function Base.display(d::ElectronDisplayType, ::MIME{Symbol("application/vnd.vegalite.v2+json")}, x)
+    payload = stringmime(MIME("application/vnd.vegalite.v2+json"), x)
+
+    html_page = """
+    <html>
+
+    <head>
+        <script src="file:///$(asset("vega.min.js"))"></script>
+        <script src="file:///$(asset("vega-lite.min.js"))"></script>
+        <script src="file:///$(asset("vega-embed.min.js"))"></script>
+    </head>
+    <body>
+      <div id="plotdiv"></div>
+    </body>
+
+    <style media="screen">
+      .vega-actions a {
+        margin-right: 10px;
+        font-family: sans-serif;
+        font-size: x-small;
+        font-style: italic;
+      }
+    </style>
+
+    <script type="text/javascript">
+
+      var opt = {
+        mode: "vega-lite",
+        actions: false
+      }
+
+      var spec = $payload
+
+      vegaEmbed('#plotdiv', spec, opt);
+
+    </script>
+
+    </html>
+    """
+
+    w = Electron.Window(html_page, options=Dict("webPreferences" => Dict("webSecurity" => false)))
+end
+
+Base.displayable(d::ElectronDisplayType, ::MIME{Symbol("application/vnd.vegalite.v2+json")}) = true
 
 function Base.display(d::ElectronDisplayType, x)
-    # if mimewritable("text/html", x)
-    #     display(d,"text/html", x)
-    if mimewritable("image/svg+xml", x)
-        display(d,MIME("image/svg+xml"), x)
-    # elseif mimewritable("image/png", x)
-    #     display(d,"image/png", x)
+    if mimewritable("application/vnd.vegalite.v2+json", x)
+        display(d,MIME("application/vnd.vegalite.v2+json"), x)
+    elseif mimewritable("image/svg+xml", x)
+        display(d,"image/svg+xml", x)
+    elseif mimewritable("image/png", x)
+        display(d,"image/png", x)
     else
         throw(MethodError(Base.display,(d,x)))
     end
 end
 
 function __init__()
-    # atreplinit(i->Base.Multimedia.pushdisplay(InlineDisplay()))
     Base.Multimedia.pushdisplay(ElectronDisplayType())
 end
 
