9f37dd564a9a739914911f887391625335592eb6
David Anthoff <anthoff@berkeley.edu>
2019-02-23 11:58:18 -0800

    Add doc template

---
+++ docs/make.jl
@@ -0,0 +1,14 @@
+using Documenter, ElectronDisplay
+
+makedocs(
+	modules = [ElectronDisplay],
+	sitename = "ElectronDisplay.jl",
+	analytics="UA-132838790-1",
+	pages = [
+        "Introduction" => "index.md"
+    ]
+)
+
+deploydocs(
+    repo = "github.com/queryverse/ElectronDisplay.jl.git"
+)
