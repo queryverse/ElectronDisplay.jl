289fc69eb8db067e99fc9a860495b0dc95709cda
Takafumi Arakaki <aka.tkf@gmail.com>
2019-10-11 17:30:58 -0700

    Add max_json_bytes to avoid sending large JSON

--- src/vega.jl
+++ src/vega.jl
@@ -4,8 +4,22 @@ Base.displayable(d::ElectronDisplayType, ::MIME{Symbol("application/vnd.vega.v3+
 Base.displayable(d::ElectronDisplayType, ::MIME{Symbol("application/vnd.vega.v4+json")}) = true
 Base.displayable(d::ElectronDisplayType, ::MIME{Symbol("application/vnd.vega.v5+json")}) = true
 
+function _maybe_fallback_to(mime, d, x, payload)
+    if sizeof(payload) > d.config.max_json_bytes
+        @warn string(
+            "The size of JSON representation (", sizeof(payload), ")",
+            " exceeds `max_json_bytes = ", d.config.max_json_bytes, "`.",
+            " Falling back to $mime."
+        )
+        return display(d, mime, x)
+    end
+    return nothing
+end
+
 function _display_vegalite(d, major_version_vegalite, major_version_vega, x)
     payload = stringmime(MIME("application/vnd.vegalite.v$major_version_vegalite+json"), x)
+    ans = _maybe_fallback_to(MIME"image/png"(), d, x, payload)
+    ans === nothing || return ans
 
     html_page = """
     <html>
@@ -49,6 +63,8 @@ end
 
 function _display_vega(d, major_version, x)
     payload = stringmime(MIME("application/vnd.vega.v$major_version+json"), x)
+    ans = _maybe_fallback_to(MIME"image/png"(), d, x, payload)
+    ans === nothing || return ans
 
     html_page = """
     <html>
