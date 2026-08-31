d520013ccbacdbf6e2737e97ace13d9241c97316
Documenter.jl <documenter@juliadocs.github.io>
2026-08-31 20:39:01 +0000

    build based on a3f016d

--- dev/search_index.js
+++ dev/search_index.js
@@ -1,3 +1,3 @@
 var documenterSearchIndex = {"docs":
-[{"location":"#Introduction-1","page":"Introduction","title":"Introduction","text":"","category":"section"}]
+[{"category":"section","location":"#Introduction","page":"Introduction","text":"","title":"Introduction"}]
 }
